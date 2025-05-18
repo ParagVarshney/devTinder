f7e74233a7fc590c588adc3a4f27981595c2930b
parag <varshneychirag321@gmail.com>
2025-05-18 14:06:04 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ const app=express();
 app.use("/test",(req,res)=>{
     res.send('hello server i am parag');
 })
+app.get("/user",(req,res)=>{
+    res.send({firstname:"parag"});
+})
 app.use("/hello",(req,res)=>{
     res.send('namaste');
+})
+app.post("/user",(req,res)=>{
+    res.send('saving database');
 })
 app.listen(2222, () => {
     console.log('Server is running on port 2222');
